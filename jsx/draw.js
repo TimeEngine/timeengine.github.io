86243a8d9b5cbeeeda9445da83dcaaa95e9d3f2d
kenokabe <kenokabe@gmail.com>
2016-05-06 20:12:58 +0900

    commit

--- jsx/draw.js
+++ jsx/draw.js
@@ -20,7 +20,7 @@ setTimeout(() => {
     __mouseDown.t = 1;
     __drawFrom.t = {
       x: e.clientX - 37,
-      y: e.clientY - 205
+      y: e.clientY - 204
     };
   };
   canvas.onmouseup = (e) => {
@@ -30,7 +30,7 @@ setTimeout(() => {
     if (__mouseDown.t === 1) {
       __drawTo.t = {
         x: e.clientX - 37,
-        y: e.clientY - 205
+        y: e.clientY - 204
       };
       __drawFrom.t = __drawTo.t;
 
