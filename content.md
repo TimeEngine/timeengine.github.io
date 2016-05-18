42143c5cd62a43c91b48f1591b3c0880258bb980
kenokabe <kenokabe@gmail.com>
2016-05-18 23:01:28 +0900

    commit

--- content.md
+++ content.md
@@ -643,6 +643,50 @@ canvas.onmousemove = (e) => {
 -----
 
 
+-----
+
+#### `Demo #10`
+
+ToDo List (React & timeengine-react)
+
+```js
+(() => {
+  'use strict';
+  const TodoElement = () => {
+    const ClockElement = () => __Element(__
+      .intervalSeq(Immutable.Range(), 100)
+      .__(() => (<div>{moment().format('MMMM Do YYYY, h:mm:ss a')}</div>)));
+    const __items = __(true);
+    const ListElement = __Element(__([__items])
+      .__(() => ((__items).map((item) => (<li>{item}</li>)))));
+    const InputElement = () => {
+      const __value = __();
+      const onChange = (e) => (__value.t = e.target.value);
+      const onClick = () => (__.log.t = __items.t = __value.t);
+      const __seqEl = __([__value])
+        .__((value) => (<div>
+        <input type="text" value={value} onChange={onChange}/>
+        <button onClick = {onClick}>{'NewToDo#' + (__items.length + 1)}</button></div>));
+      __.log.__(() => (__value.t = ""));
+      __.log.t = "started!";
+      return (<div>{__Element(__seqEl)}</div>);
+    };
+    return (<div><h2>ToDo</h2>
+      {ClockElement()}<p/>
+      {ListElement}
+      {InputElement()}</div>);
+  };
+  const mount = ReactDOM.render(TodoElement(), document.getElementById('todo'));
+})();
+```
+
+<div id="todo"</div>
+
+
+<script src ="./jsx/todolist.js"></script>
+
+-------------------
+
 <script src ="./jsx/div-react.js"></script>
 
 
---
+++ jsx/todolist.js
@@ -0,0 +1,83 @@
+setTimeout(() => {
+
+  //------
+  (function() {
+    'use strict';
+
+    var TodoElement = function TodoElement() {
+      var ClockElement = function ClockElement() {
+        return __Element(__.intervalSeq(Immutable.Range(), 100).__(function() {
+          return React.createElement(
+            'div',
+            null,
+            moment().format('MMMM Do YYYY, h:mm:ss a')
+          );
+        }));
+      };
+      var __items = __(true);
+      var ListElement = __Element(__([__items]).__(function() {
+        return __items.map(function(item) {
+          return React.createElement(
+            'li',
+            null,
+            item
+          );
+        });
+      }));
+      var InputElement = function InputElement() {
+        var __value = __();
+        var onChange = function onChange(e) {
+          return __value.t = e.target.value;
+        };
+        var onClick = function onClick() {
+          return __.log.t = __items.t = __value.t;
+        };
+        var __seqEl = __([__value]).__(function(value) {
+          return React.createElement(
+            'div',
+            null,
+            React.createElement('input', {
+              type: 'text',
+              value: value,
+              onChange: onChange
+            }),
+            React.createElement(
+              'button',
+              {
+                onClick: onClick
+              },
+              'NewToDo#' + (__items.length + 1)
+            )
+          );
+        });
+        __.log.__(function() {
+          return __value.t = "";
+        });
+        __.log.t = "started!";
+        return React.createElement(
+          'div',
+          null,
+          __Element(__seqEl)
+        );
+      };
+      return React.createElement(
+        'div',
+        null,
+        React.createElement(
+          'h2',
+          null,
+          'ToDo'
+        ),
+        ClockElement(),
+        React.createElement('p', null),
+        ListElement,
+        InputElement()
+      );
+    };
+    var mount = ReactDOM.render(TodoElement(), document.getElementById('todo'));
+  })();
+
+
+  //---------
+
+}, 1000);
