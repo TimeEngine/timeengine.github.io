53b29c7db0a253c437af344e180d899899507496
kenokabe <kenokabe@gmail.com>
2016-05-22 22:00:57 +0900

    commit

--- jsx/todolist2.js
+++ jsx/todolist2.js
@@ -2,27 +2,35 @@
 setTimeout(() => {
 
 
-
+  /*global React ReactDOM __ Immutable __Element*/
   (function() {
     'use strict';
 
     var TodoElement = function TodoElement() {
-      var ClockElement = function ClockElement() {
-        return __Element(__.intervalSeq(Immutable.Range(), 100).__(function() {
+      var ClockElement = __Element(__.intervalSeq(Immutable.Range(), 100).__(function() {
+        return React.createElement(
+          'div',
+          null,
+          moment().format('MMMM Do YYYY, h:mm:ss a')
+        );
+      }));
+      var __focusedList = __().__(function(val) {
+        return __.log.t = val;
+      });
+      var __lists = __(true).__(function(__list) {
+        __focusedList.t = __lists.length;
+        return __list;
+      });
+      var ListofListElement = __Element(__([__lists]).__(function() {
+        return __lists.map(function(list, i) {
           return React.createElement(
-            'div',
-            null,
-            moment().format('MMMM Do YYYY, h:mm:ss a')
-          );
-        }));
-      };
-      var __items = __(true);
-      var ListElement = __Element(__([__items]).__(function() {
-        return __items.map(function(item) {
-          return React.createElement(
-            'li',
-            null,
-            item
+            'button',
+            {
+              onClick: function onClick() {
+                return __.log.t = __focusedList.t = i;
+              }
+            },
+            'List#' + (i + 1)
           );
         });
       }));
@@ -32,12 +40,35 @@ setTimeout(() => {
           return __value.t = e.target.value;
         };
         var onClick = function onClick() {
-          return __.log.t = __items.t = __value.t;
+          __.log.t = __lists[__focusedList.t].t = __value.t;
+        };
+        var onClickNL = function onClickNL() {
+          __lists.t = __(true); //new items-list
+          __.log.t = 'newList';
         };
         var __seqEl = __([__value]).__(function(value) {
           return React.createElement(
             'div',
             null,
+            React.createElement(
+              'h4',
+              null,
+              'List#' + (__focusedList.t + 1),
+              React.createElement(
+                'button',
+                {
+                  onClick: onClickNL
+                },
+                'NewList'
+              )
+            ),
+            __lists[__focusedList.t].map(function(item) {
+              return React.createElement(
+                'li',
+                null,
+                item
+              );
+            }),
             React.createElement('input', {
               type: 'text',
               value: value,
@@ -48,20 +79,21 @@ setTimeout(() => {
               {
                 onClick: onClick
               },
-              'NewToDo#' + (__items.length + 1)
+              'NewToDo#' + (__lists[__focusedList.t].length + 1)
             )
           );
         });
         __.log.__(function() {
           return __value.t = "";
         });
-        __.log.t = "started!";
-        return React.createElement(
-          'div',
-          null,
-          __Element(__seqEl)
-        );
+        return __Element(__seqEl);
       };
+      __lists.t = __(true); //new items-list
+      (function() {
+        var __delay = __.intervalSeq(Immutable.Seq.of("started!"), 1000).log().__(function() {
+          return __.log.t = "showInput";
+        });
+      })();
       return React.createElement(
         'div',
         null,
@@ -70,15 +102,15 @@ setTimeout(() => {
           null,
           'ToDo'
         ),
-        ClockElement(),
+        ClockElement,
         React.createElement('p', null),
-        ListElement,
+        ListofListElement,
+        React.createElement('p', null),
         InputElement()
       );
     };
-    var mount = ReactDOM.render(TodoElement(), document.getElementById('todo2'));
+    var mount = ReactDOM.render(TodoElement(), document.getElementById('todo'));
   })();
-
   //---------
 
 }, 1000);
