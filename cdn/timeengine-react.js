8b1a2654607d0de5fcc37e444031391a6ef367d4
kenokabe <kenokabe@gmail.com>
2017-03-12 16:33:31 +0900

    commit

--- cdn/timeengine-react.js
+++ cdn/timeengine-react.js
@@ -1,68 +1,138 @@
-/*global React timeengine __ Immutable __Element*/
+"use strict";
 
+var _slicedToArray = function() {
+  function sliceIterator(arr, i) {
+    var _arr = [];
+    var _n = true;
+    var _d = false;
+    var _e = undefined;
+    try {
+      for (var _i = arr[Symbol.iterator](), _s; !(_n = (_s = _i.next()).done); _n = true) {
+        _arr.push(_s.value);
+        if (i && _arr.length === i) break;
+      }
+    } catch (err) {
+      _d = true;
+      _e = err;
+    } finally {
+      try {
+        if (!_n && _i["return"]) _i["return"]();
+      } finally {
+        if (_d)
+          throw _e;
+      }
+    }
+    return _arr;
+  }
+  return function(arr, i) {
+    if (Array.isArray(arr)) {
+      return arr;
+    } else if (Symbol.iterator in Object(arr)) {
+      return sliceIterator(arr, i);
+    } else {
+      throw new TypeError("Invalid attempt to destructure non-iterable instance");
+    }
+  };
+}();
+
+var _createClass = function() {
+  function defineProperties(target, props) {
+    for (var i = 0; i < props.length; i++) {
+      var descriptor = props[i];
+      descriptor.enumerable = descriptor.enumerable || false;
+      descriptor.configurable = true;
+      if ("value" in descriptor)
+        descriptor.writable = true;
+      Object.defineProperty(target, descriptor.key, descriptor);
+    }
+  }
+  return function(Constructor, protoProps, staticProps) {
+    if (protoProps) defineProperties(Constructor.prototype, protoProps);
+    if (staticProps) defineProperties(Constructor, staticProps);
+    return Constructor;
+  };
+}();
+
+function _classCallCheck(instance, Constructor) {
+  if (!(instance instanceof Constructor)) {
+    throw new TypeError("Cannot call a class as a function");
+  }
+}
+
+function _possibleConstructorReturn(self, call) {
+  if (!self) {
+    throw new ReferenceError("this hasn't been initialised - super() hasn't been called");
+  }
+  return call && (typeof call === "object" || typeof call === "function") ? call : self;
+}
+
+function _inherits(subClass, superClass) {
+  if (typeof superClass !== "function" && superClass !== null) {
+    throw new TypeError("Super expression must either be null or a function, not " + typeof superClass);
+  }
+  subClass.prototype = Object.create(superClass && superClass.prototype, {
+    constructor: {
+      value: subClass,
+      enumerable: false,
+      writable: true,
+      configurable: true
+    }
+  });
+  if (superClass)
+    Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass;
+}
+
+/*global React timeengine __ Immutable __Element*/
 //this file need to be transpiled to js
 //npm test
-'use strict';
+(function() {
+  "use strict";
 
-var _createClass = (function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ('value' in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; })();
+  var mode = typeof module !== "undefined" && module.exports;
+  var React = mode ? require("react") : window.React;
+  var ReactDOM = mode ? require("react-dom") : window.ReactDOM;
+  var __ = mode ? require("timeengine") : window.__;
 
-var _get = function get(_x2, _x3, _x4) { var _again = true; _function: while (_again) { var object = _x2, property = _x3, receiver = _x4; _again = false; if (object === null) object = Function.prototype; var desc = Object.getOwnPropertyDescriptor(object, property); if (desc === undefined) { var parent = Object.getPrototypeOf(object); if (parent === null) { return undefined; } else { _x2 = parent; _x3 = property; _x4 = receiver; _again = true; desc = parent = undefined; continue _function; } } else if ('value' in desc) { return desc.value; } else { var getter = desc.get; if (getter === undefined) { return undefined; } return getter.call(receiver); } } };
-
-function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError('Cannot call a class as a function'); } }
-
-function _inherits(subClass, superClass) { if (typeof superClass !== 'function' && superClass !== null) { throw new TypeError('Super expression must either be null or a function, not ' + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
-
-(function () {
-  'use strict';
-
-  var React = undefined;
-  var ReactDOM = undefined;
-  var __ = undefined;
-  if (typeof module !== 'undefined' && module.exports) {
-    React = require("react");
-    ReactDOM = require("react-dom");
-    __ = require("timeengine");
-  } else {
-    React = window.React;
-    ReactDOM = window.ReactDOM;
-    __ = window.__;
-  }
   //***React state with life cycle is stateless sequence*****
   var __Element = function __Element(__seqEl) {
-    var f = arguments.length <= 1 || arguments[1] === undefined ? function () {
+    var f = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : function() {
       return 0;
-    } : arguments[1];
+    };
 
-    var SeqComponent = (function (_React$Component) {
+    var SeqComponent = function(_React$Component) {
       _inherits(SeqComponent, _React$Component);
 
       function SeqComponent() {
-        var _this = this;
-
         _classCallCheck(this, SeqComponent);
 
-        _get(Object.getPrototypeOf(SeqComponent.prototype), 'constructor', this).call(this);
-        this.state = {
+        var _this = _possibleConstructorReturn(this, (SeqComponent.__proto__ || Object.getPrototypeOf(SeqComponent)).call(this));
+
+        _this.state = {
           seqEl: __seqEl.t
         };
-        var timeseq = __seqEl.__(function (val) {
+        var __timeseq = __([__seqEl]).__(function(_ref) {
+          var _ref2 = _slicedToArray(_ref, 1),
+            val = _ref2[0];
+
           _this.setState({
             seqEl: val
           });
           f(ReactDOM.findDOMNode(_this).children[0]);
+          return true;
         });
+        return _this;
       }
 
       _createClass(SeqComponent, [{
-        key: 'componentWillUnmount',
+        key: "componentWillUnmount",
         value: function componentWillUnmount() {
           __seqEl.done = 1;
         }
       }, {
-        key: 'render',
+        key: "render",
         value: function render() {
           return React.createElement(
-            'span',
+            "span",
             null,
             this.state.seqEl
           );
@@ -70,15 +140,14 @@ function _inherits(subClass, superClass) { if (typeof superClass !== 'function' 
       }]);
 
       return SeqComponent;
-    })(React.Component);
+    }(React.Component);
 
     return React.createElement(SeqComponent, null);
   };
   //------------------
-
-  if (typeof module !== 'undefined' && module.exports) {
+  var binder = mode ? function() {
     module.exports = __Element;
-  } else {
+  }() : function() {
     window.__Element = __Element;
-  }
+  }();
 })();
--- cdn/timeengine.js
+++ cdn/timeengine.js
@@ -1,32 +1,30 @@
 "use strict";
 
-(function () {
-  'use strict';
+(function() {
+  "use strict";
 
-  var getID = function (id0) {
+  var getID = function(id0) {
     var id = id0;
-    return function () {
+    return function() {
       return id++;
     };
   }(0);
   //-----------------------------------
   //__([a,b], true) // new seq depenus on us =[a,b]
   var __ = function __() {
-    var us = arguments.length <= 0 || arguments[0] === undefined ? [] : arguments[0];
-    var store = arguments.length <= 1 || arguments[1] === undefined ? false : arguments[1];
+    var us = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : [];
+    var store = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : false;
 
-    if (typeof us === "boolean") {
-      store = us;
-      us = [];
-    }
+    store = typeof us === "boolean" ? us : store;
+    us = typeof us === "boolean" ? [] : us;
+
     var seq = []; //seq is vanilla JS array + features
     seq.id = getID();
     seq.store = store;
     seq.us = us;
     seq.ds = [];
-
     seq.updatedTo = {};
-    seq.us.map(function (u) {
+    seq.us.map(function(u) {
       //  seq is a member of u.ds
       u.ds[u.ds.length] = seq;
       u.updatedTo[seq.id] = 0; // non-interference dependency
@@ -36,98 +34,97 @@
     seq.propagating = 0;
     seq.done = 0;
     seq.eqs = [];
-    seq.addEq = function (eq) {
+    seq.addEq = function(eq) {
       return seq.eqs[seq.eqs.length] = eq;
     };
-    seq.evalEqs = function (val) {
+    seq.evalEqs = function(val) {
       var val0 = val;
-      seq.eqs.map(function (eq) {
+      seq.eqs.map(function(eq) {
         return val = eq(val);
       });
       return seq.us.length === 0 ? val0 : val;
     };
     //api-----------------
-    Object.keys(__.api).map(function (api) {
+    Object.keys(__.api).map(function(api) {
       seq[api] = __.api[api](__, seq, store);
     });
     //-----------------
     Object.defineProperties(seq, //detect t update on each seqs
-    {
-      t: { //foo.t
-
-        get: function get() {
-          return seq.valOnT;
-        },
-        set: function set(tval) {
-          if (seq.propagating === 0 && //sanity check
-          seq.us.length !== 0) {
-            throw new Error("cannot set a value on sequence that depenus on other sequences");
-          } else {
-            seq.propagating = 0;
-            if (seq.done === 0) {
-              seq.valOnT = seq.evalEqs(tval); //self eqs eval
-              seq.T = Date.now();
-              if (store) {
-                seq.IndexOnTimestamp[seq.T] = seq.length;
-                seq.TimestampOnIndex[seq.length] = seq.T;
-                seq[seq.length] = seq.valOnT; //after funcs
-              }
-              Object.keys(seq.updatedTo).map(function (key) {
-                seq.updatedTo[key] = 1;
-              });
-              //clear updated us in non-interference way
-              seq.us.map(function (u) {
-                return u.updatedTo[seq.id] = 0;
-              });
-              seq.ds.map(function (d) {
-                var usAllUpdated = d.us.map(function (u) {
-                  return u.updatedTo[d.id];
-                }).reduce(function (a, b) {
-                  return a * b;
+      {
+        t: {
+          //foo.t
+          get: function get() {
+            return seq.valOnT;
+          },
+          set: function set(tval) {
+            var sanityCheck = seq.propagating === 0 && seq.us.length !== 0 ? function() {
+              throw new Error("Do not set a value of the sequence that depends on other sequences!");
+            }() : function() {
+              seq.propagating = 0;
+              var core = seq.done === 0 ? function() {
+                seq.valOnT = seq.evalEqs(tval); //self eqs eval
+                seq.T = Date.now();
+                var core1 = store ? function() {
+                  seq.IndexOnTimestamp[seq.T] = seq.length;
+                  seq.TimestampOnIndex[seq.length] = seq.T;
+                  seq[seq.length] = seq.valOnT; //after funcs
+                }() : true;
+                Object.keys(seq.updatedTo).map(function(key) {
+                  seq.updatedTo[key] = 1;
                 });
-                if (usAllUpdated === 1) {
-                  d.propagating = 1;
-                  d.t = d.us.map(function (u) {
-                    return u.t;
+                //clear updated us in non-interference way
+                seq.us.map(function(u) {
+                  return u.updatedTo[seq.id] = 0;
+                });
+                seq.ds.map(function(d) {
+                  var usAllUpdated = d.us.map(function(u) {
+                    return u.updatedTo[d.id];
+                  }).reduce(function(a, b) {
+                    return a * b;
                   });
-                }
-              });
-            }
+                  var core2 = usAllUpdated === 1 ? function() {
+                    d.propagating = 1;
+                    d.t = d.us.map(function(u) {
+                      return u.t;
+                    });
+                  }() : true;
+                });
+              }() : true;
+            }();
           }
         }
-      }
-    });
+      });
     return seq;
   };
   //==================
   __.api = {};
   //--------------------------------------
-  __.api.__ = function (__, seq, store) {
-    return function (f) {
-      seq.addEq(function (val) {
+  __.api.__ = function(__, seq, store) {
+    return function(f) {
+      seq.addEq(function(val) {
         return f(val, seq.T);
       });
       return seq;
     };
   };
 
-  __.api.log = function (__, seq, store) {
-    return function (msg) {
-      seq.addEq(function (val) {
-        if (typeof msg === "undefined") {
-          console.log(val);
+  __.api.log = function(__, seq, store) {
+    return function(msg) {
+      seq.addEq(function(val) {
+        return typeof msg === "undefined" ? function() {
+          console.info(">>", val);
           return val;
-        } else {
+        }() : function() {
           console.info(msg + ":", val);
           return val;
-        }
+        }();
       });
       return seq;
     };
   };
 
-  __.api.onT = function (__, seq, store) {
-    return function (timestamp) {
+  __.api.onT = function(__, seq, store) {
+    return function(timestamp) {
       if (store) {
         return seq[seq.IndexOnTimestamp[timestamp]];
       } else {
@@ -137,29 +134,23 @@
   };
 
   //top level api
-  __.log = __([], true).__(function (val) {
-    console.info(">>> ", val);
+  __.log = __([], true).__(function(val) {
+    console.info(">>", val);
     return val;
   });
 
-  __.intervalSeq = function (immutableSeq, interval) {
-    var store = arguments.length <= 2 || arguments[2] === undefined ? false : arguments[2];
+  __.intervalSeq = function(immutableSeq, interval) {
+    var store = arguments.length > 2 && arguments[2] !== undefined ? arguments[2] : false;
 
     var seq = __([], store);
     var it = immutableSeq.values();
     var val = __();
     val.t = it.next().value;
-    if (typeof val.t !== "undefined") {
-      (function () {
-        var timer = setInterval(function () {
-          seq.t = val.t;
-          val.t = it.next().value;
-          if (typeof val.t === "undefined") {
-            clearInterval(timer);
-          }
-        }, interval);
-      })();
-    }
+    var timer = typeof val.t !== "undefined" ? setInterval(function() {
+      seq.t = val.t;
+      val.t = it.next().value;
+      var stop = typeof val.t === "undefined" ? clearInterval(timer) : true;
+    }, interval) : true;
     return seq;
   };
 
@@ -174,7 +165,7 @@
     }
   });
 
-  __.pure = function (legacyF) {
+  __.pure = function(legacyF) {
     var f = function f() {
       for (var _len = arguments.length, args = Array(_len), _key = 0; _key < _len; _key++) {
         args[_key] = arguments[_key];
@@ -188,10 +179,10 @@
     return f;
   };
   //------------------
-  if (typeof module !== 'undefined' && module.exports) {
+  if (typeof module !== "undefined" && module.exports) {
     module.exports = __;
   } else {
     window.__ = __;
   }
-  //============================
+//============================
 })();
